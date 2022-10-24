3dd92730557dadfb40ee28f6a0041e636876b4a7
Pau <pauramisnunez@gmail.com>
2022-10-24 15:59:51 +0200

    1 i 3 cmplt

--- src/Exercici3.java
+++ src/Exercici3.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 
-public class Exercici1 {
-    static Turtle t = new Turtle(500,500);
+public class Exercici3 {
 
+    static Turtle t = new Turtle(500,500);
+    static int cLenght = 60;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Caudrats en horitzontal: ");
         int cuadratsHorizontal = scanner.nextInt();
         System.out.println("Cuadrats en horitzontal: ");
         int cuadratsVertical = scanner.nextInt();
-        System.out.println("Longitut costats");
-        int cLenght = scanner.nextInt();
 
         for (int i = 0; i < cuadratsVertical; i++) {
-            hRow(cuadratsHorizontal, cLenght);
-            reset(cuadratsHorizontal, cLenght);
-            prepareNext(cLenght);
+            hRow(cuadratsHorizontal);
+            reset(cuadratsHorizontal);
+            prepareNext();
         }
 
         // Show cursor
@@ -25,36 +24,34 @@ public class Exercici1 {
         t.show();
     }
 
-    private static void prepareNext(int cLenght) {
-        t.setPenDown(false);
-        t.forward(cLenght*2);
-        t.setPenDown(true);
+    private static void prepareNext() {
+        t.forward(cLenght);
     }
 
-    private static void hRow(int cuadratsHorizontal, int cLenght) {
+    private static void hRow(int cuadratsHorizontal) {
         for (int i = 0; i < cuadratsHorizontal; i++) {
-            cuadrat(cLenght);
-            next(cLenght);
+            cuadrat();
+            next();
         }
     }
 
-    private static void reset(int cuadratsHorizontal, int cLenght) {
+    private static void reset(int cuadratsHorizontal) {
         t.setPenDown(false);
         t.turnRight(90);
-        t.forward(cLenght * cuadratsHorizontal * 2);
+        t.forward(cLenght * cuadratsHorizontal);
         t.turnLeft(90);
         t.setPenDown(true);
     }
 
-    private static void next(int cLenght) {
+    private static void next() {
         t.setPenDown(false);
-        t.forward(cLenght*2);
+        t.forward(cLenght);
         t.turnRight(90);
         t.setPenDown(true);
     }
 
 
-    private static void cuadrat(int cLenght){
+    private static void cuadrat(){
         for (int i = 0; i < 3; i++) {
             t.forward(cLenght);
             t.turnRight(90);
